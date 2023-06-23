bcb82e0206b3fbd5cf0a044a7c46bcf5bef4d0df
Aditya Shukla <107319226+Aditya12047@users.noreply.github.com>
2023-06-23 12:55:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 # Send a GET request to the website
-url = "https://example.com"  # Replace with the URL of the website you want to scrape
+url = "https://example.com"  
 response = requests.get(url)
 
-# Create a BeautifulSoup object to parse the HTML content
+
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find specific elements on the page using CSS selectors
-# Replace "selector" with the CSS selector of the elements you want to scrape
+
 elements = soup.select("selector")
 
-# Process and extract the desired data from the elements
+
 for element in elements:
-    # Extract text or attribute values from the element
-    # Replace "attribute_name" with the actual attribute name you want to extract
+  
     data = element.get("attribute_name")
     
-    # Process the extracted data as needed
+
     print(data)
